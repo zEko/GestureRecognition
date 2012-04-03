42d7da3c803209ac54967f620f8ddb6b33cc6fc7
zEko Frost <zeko.frost@gmail.com>
2012-04-03 18:54:49 +0530

    Semantic issues

--- SignRec.m
+++ SignRec.m
@@ -15,6 +15,7 @@ clc; clf; clear all; close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Image acquisition
 %
+
 background_image = imread('bg.jpg');
 image_in = imread('hand.jpg');
 
@@ -235,35 +236,35 @@ middle = 3;
 index = 4;
 thumb = 5;
 
-pinky_total = numel(pinky_mat > 0);
+pinky_total = numel(pinky_mat);
 pinky_threshold = 50;
 pinky_true = (((numel(find(pinky_mat > 0)))/pinky_total)*100) > pinky_threshold;
 if(pinky_true)
   finger_mat(pinky) = 1;
 end
 
-ring_total = numel(ring_mat > 0);
+ring_total = numel(ring_mat);
 ring_threshold = 50;
 ring_true = (((numel(find(ring_mat > 0)))/ring_total)*100) > ring_threshold;
 if(ring_true)
   finger_mat(ring) = 1;
 end
 
-middle_total = numel(middle_mat > 0);
+middle_total = numel(middle_mat);
 middle_threshold = 50;
 middle_true = (((numel(find(middle_mat > 0)))/middle_total)*100) > middle_threshold;
 if(middle_true)
   finger_mat(middle) = 1;
 end
 
-index_total = numel(index_mat > 0);
+index_total = numel(index_mat);
 index_threshold = 50;
 index_true = (((numel(find(index_mat > 0)))/index_total)*100) > index_threshold;
 if(index_true)
   finger_mat(index) = 1;
 end
 
-thumb_total = numel(thumb_mat > 0);
+thumb_total = numel(thumb_mat);
 thumb_threshold = 50;
 thumb_true = (((numel(find(thumb_mat > 0)))/thumb_total)*100) > thumb_threshold;
 if(thumb_true)
