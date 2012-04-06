af0a31deb8458db5462d6a36088d57e946540d03
zEko Frost <zeko.frost@gmail.com>
2012-04-07 01:43:37 +0530

    Moved Thumb matrix away from the bottom of palm Index region was increased, since sign two was causing troubles

--- FingerMatrix.m
+++ FingerMatrix.m
@@ -21,13 +21,13 @@ middle_top = middle_bottom+30;
 middle_mat = object_image(middle_bottom:middle_top, middle_right:middle_left);
 
 index_left = middle_right;
-index_right = index_left-13;
+index_right = index_left-25;
 index_bottom = middle_bottom+5; % index_offset
 index_top = index_bottom+20;
 index_mat = object_image(index_bottom:index_top, index_right:index_left);
 
 thumb_left = start_of_palm(2)-10;
 thumb_right = thumb_left-30;
-thumb_bottom = start_of_palm(1); % thumb_offset
+thumb_bottom = start_of_palm(1)+10; % thumb_offset
 thumb_top = thumb_bottom+30;
 thumb_mat = object_image(thumb_bottom:thumb_top, thumb_right:thumb_left);
