1ea7bbb0231c11bb7cfb6d61f2ee6c837fb41c3b
zEko Frost <zeko.frost@gmail.com>
2012-05-01 00:46:40 +0530

    Changed the dimensions of finger boxes

--- FingerMatrix.m
+++ FingerMatrix.m
@@ -2,32 +2,38 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% Finger Matrix %%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pinky_left = end_of_palm(2);
-pinky_right = pinky_left-10;
+pinky_left = pinky_right+palm_width;
+if(pinky_left > nc)
+  pinky_left = nc;
+end
+pinky_right = end_of_palm(2) - round(palm_width/4) ;
 pinky_bottom = end_of_palm(1)+palm_height;
-pinky_top = pinky_bottom+30;
+pinky_top = pinky_bottom+palm_height;
 pinky_mat = object_image(pinky_bottom:pinky_top, pinky_right:pinky_left);
 
-ring_left = pinky_right;
-ring_right = ring_left-10;
-ring_bottom = pinky_bottom+5; % ring_offset
-ring_top = ring_bottom+30;
+ring_left = pinky_right-3;
+ring_right = start_of_palm(2) + round(palm_width/2) - 5;
+ring_bottom = pinky_bottom;
+ring_top = ring_bottom+palm_height;
 ring_mat = object_image(ring_bottom:ring_top, ring_right:ring_left);
 
-middle_left = ring_right;
-middle_right = middle_left-10;
-middle_bottom = ring_bottom+5; % middle_offset
-middle_top = middle_bottom+30;
+middle_left = ring_right - 5;
+middle_right = middle_left - round(palm_width/4) - round(palm_width/8);
+middle_bottom = pinky_bottom;
+middle_top = middle_bottom+palm_height;
 middle_mat = object_image(middle_bottom:middle_top, middle_right:middle_left);
 
 index_left = middle_right;
-index_right = index_left-25;
-index_bottom = middle_bottom+5; % index_offset
-index_top = index_bottom+20;
+index_right = index_left-palm_width;
+index_bottom = middle_bottom;
+index_top = index_bottom+palm_height;
 index_mat = object_image(index_bottom:index_top, index_right:index_left);
 
-thumb_left = start_of_palm(2)-10;
-thumb_right = thumb_left-30;
-thumb_bottom = start_of_palm(1)+10; % thumb_offset
+thumb_left = start_of_palm(2)-5;
+thumb_right = thumb_left-palm_height;
+if(thumb_right < 1)
+  thumb_right = 1;
+end
+thumb_bottom = start_of_palm(1);
 thumb_top = thumb_bottom+30;
 thumb_mat = object_image(thumb_bottom:thumb_top, thumb_right:thumb_left);
