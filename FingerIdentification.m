384a8c4a2041838171ab8a9578a221ccbe7c05e8
zEko Frost <zeko.frost@gmail.com>
2012-05-01 00:47:01 +0530

    Change of thresholds

--- FingerIdentification.m
+++ FingerIdentification.m
@@ -11,31 +11,31 @@ thumb = 5;
 
 pinky_total = numel(pinky_mat);
 pinky_true = numel(find(pinky_mat == 1));
-if(pinky_true > 50)
+if(pinky_true > 200)
   finger_mat(pinky) = 1;
 end
 
 ring_total = numel(ring_mat);
 ring_true = numel(find(ring_mat == 1));
-if(ring_true > 50)
+if(ring_true > 250)
   finger_mat(ring) = 1;
 end
 
 middle_total = numel(middle_mat);
 middle_true = numel(find(middle_mat == 1));
-if(middle_true > 50)
+if(middle_true > 250)
   finger_mat(middle) = 1;
 end
 
 index_total = numel(index_mat);
 index_true = numel(find(index_mat == 1));
-if(index_true > 50)
+if(index_true > 250)
   finger_mat(index) = 1;
 end
 
 thumb_total = numel(thumb_mat);
 thumb_true = numel(find(thumb_mat == 1));
-if(thumb_true > 90)
+if(thumb_true > 250)
   finger_mat(thumb) = 1;
 end
 
